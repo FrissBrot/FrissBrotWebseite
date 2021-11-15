60a896f93dfc98b99405ad7b77a1e6bc05db1f28
FrissBrot <60073321+FrissBrot@users.noreply.github.com>
2021-11-15 11:26:34 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<html lang="de">
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+    <link rel="icon" href="Bilder\favicon.png" type="image/png">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FrissBrot</title>
+    <div class=flex>
+      <img src="Bilder\LogoBanner.png">
+    </div>
+  </head>
+  <body>
+    <div>
+      <ul class="flex Text">
+        <li><a href="#home">Home</a></li>
+        <li><a href="discord.html">Discord</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+    </div>
+    <form action="index.php" method="post">
+
+    </form>
+  </body>
+</html>
